2cb56500a2b51519db57d19b524e836515a349b5
luke <luke44@gmail.com>
2021-09-21 15:44:46 +0100

    No branching proproblems, method release_bike added
    Co-authored-by: Arthur <arthurfincham@me.com>
    Co-authored-by: Luke <luke44@gmail.com>

--- spec/sourcecode_spec.rb
+++ spec/sourcecode_spec.rb
@@ -4,6 +4,6 @@ checker = DockingStation.new
 
 describe DockingStation do
   it 'instances respond to release_bike' do
-    expect(checker).to respond_to(release_bike)
+    expect(checker).to respond_to(:release_bike)
   end
 end
