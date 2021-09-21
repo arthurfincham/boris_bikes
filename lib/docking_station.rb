d8dd9b9f411c534b007aa710605f87d2f228a0c6
luke <luke44@gmail.com>
2021-09-21 16:22:59 +0100

    Bike is working
    Co-authored-by: Arthur <arthurfincham@me.com>
    Co-authored-by: Luke <luke44@gmail.com>

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,7 @@
+require './lib/bike'
+
 class DockingStation
   def release_bike
+    bike = Bike.new
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,4 +6,13 @@ describe DockingStation do
   it 'instances respond to release_bike' do
     expect(checker).to respond_to(:release_bike)
   end
+
+  it 'gets a bike' do
+    expect(checker.release_bike).to be_an_instance_of(Bike)
+  end
+
+  it 'bike works' do
+    expect(checker.release_bike.working?).to eq true
+  end
+
 end
