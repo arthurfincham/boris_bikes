5098ac94f08ad4352fd9cf731dfa239c43fb1000
luke <luke44@gmail.com>
2021-09-21 15:40:14 +0100

    Branches out of sync?

--- spec/sourcecode_spec.rb
+++ spec/sourcecode_spec.rb
@@ -1,5 +1,9 @@
 require 'sourcecode'
 
-describe DockingStation do
+checker = DockingStation.new
 
+describe DockingStation do
+  it 'instances respond to release_bike' do
+    expect(checker).to respond_to(release_bike)
+  end
 end
